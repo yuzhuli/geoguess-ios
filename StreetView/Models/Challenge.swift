593b364a84dfbf6888e13006f56582def2b05d53
Randy Zhao <randyzhao@users.noreply.github.com>
2018-02-06 00:03:47 -0800

    Add basic classes of challenge (#1)

---
+++ StreetView/Models/Challenge.swift
@@ -0,0 +1,28 @@
+//
+//  Challenge.swift
+//  StreetView
+//
+//  Created by Hongze Zhao on 2/5/18.
+//  Copyright © 2018 LiYuzhu. All rights reserved.
+//
+
+import Foundation
+
+struct Round {
+  let initialGeoLocation: GeoLocation
+}
+
+struct Challenge {
+  let name: String
+  let description: String
+  let rounds: [Round]
+}
+
+class ChallengeManager {
+  lazy var singleTestingChallenge: Challenge = {
+    return Challenge(
+      name: "single testing challenge",
+      description: "test description",
+      rounds: [Round(initialGeoLocation: GeoUtils.randomSFLocation())])
+  }()
+}
